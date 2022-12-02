e11716061974c8c4cdb32d8a18fb87cf66804dec
SeminixDev <seminixp@gmail.com>
2022-12-02 17:30:27 +0000

    2022 Day 2 Part 1 - Rock Paper Scissors

---
+++ Day2RockPaperScissors/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Day2RockPaperScissors
+{
+    public class Game
+    {
+        int elfPlay;
+        int playerPlay;
+
+        public Game(char elfPlay, char playerPlay)
+        {
+            this.elfPlay = CharToPlay(elfPlay);
+            this.playerPlay = CharToPlay(playerPlay);
+        }
+
+        static int CharToPlay(char play)
+        {
+            switch(play)
+            {
+                case 'A':
+                case 'X':
+                    return 1;
+                case 'B':
+                case 'Y':
+                    return 2;
+                case 'C':
+                case 'Z':
+                    return 3;
+                default:
+                    throw new Exception("Can't convert input");
+            }
+        }
+
+        public int GetGameResult()
+        {
+            if (elfPlay == playerPlay)
+            {
+                return playerPlay + 3; // draw
+            }
+            if (elfPlay == 1 && playerPlay == 3)
+            {
+                return playerPlay; //elf wins with rock
+            }
+            if (elfPlay == 3 && playerPlay == 1)
+            {
+                return playerPlay + 6; //player wins with rock
+            }
+            if (elfPlay > playerPlay)
+            {
+                return playerPlay; // elf wins with paper/scissors
+            }
+            return playerPlay + 6; // player wins with paper/scissors
+        }
+    }
+}
---
+++ Day2RockPaperScissors/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Day2RockPaperScissors
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetScoreFromInput());
+        }
+
+        static int GetScoreFromInput()
+        {
+            int totalScore = 0;
+            StreamReader sr = new StreamReader("Input.txt");
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line)) 
+                    totalScore += GetSingleGameScore(line[0], line[2]);
+            }
+            return totalScore;
+        }
+
+        static int GetSingleGameScore(char elfPlay, char playerPlay)
+        {
+            Game game = new Game(elfPlay, playerPlay);
+            return game.GetGameResult();
+        }
+    }
+}
